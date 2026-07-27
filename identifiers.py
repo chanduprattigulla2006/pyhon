d190708b58010a2dc20416067b77dbe1574f838e
chanduprattigulla2006 <chanduprattigulla2006@gmail.com>
2026-07-27 05:53:58 +0530

    identifiers.py

---
+++ identifiers.py
@@ -0,0 +1,22 @@
+#Program:Valid Identifiers
+#lab:01
+#task:01
+name="chandu_srinvas"
+PI=3.14
+def hello():
+    print("HELLO_WORLD")
+class grade:
+    pass
+student_marks=64
+print(name)
+print(PI)
+print(hello)
+print(grade)
+print(student_marks)
+#Lab:01
+#Task:03
+#Program:Case Sensitivity of Identifiers
+Sum=45
+sum=18
+print(Sum)
+print(sum)
