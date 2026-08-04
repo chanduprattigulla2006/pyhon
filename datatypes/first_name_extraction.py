e7548053d413c21f335f04e4a7432d05cfc9cb2d
chanduprattigulla2006 <chanduprattigulla2006@gmail.com>
2026-08-04 20:01:31 +0530

    Add first name extraction program using string slicing

---
+++ datatypes/first_name_extraction.py
@@ -0,0 +1,8 @@
+#Program:First Name Extraction
+#Task:A2.2-String Slicing
+full_name=input("Enter your full name\n")
+print("Your First name is :",full_name[:full_name.rindex(" ")])
+#output
+#Enter your full name
+#Chandu srinvas prattigulla
+#Your First name is : chandu srinvas
